c17f3fb61dc07575d74505c46dca9bd5dba1dd6f
Sebastian Rothbucher <sebastianrothbucher@googlemail.com>
2019-11-23 18:50:54 +0100

    Add server-only component

---
+++ src/server-only.js
@@ -0,0 +1,36 @@
+export default {
+  name: 'ServerOnly',
+  functional: true,
+  props: {
+    placeholder: String,
+    placeholderTag: {
+      type: String,
+      default: 'div'
+    }
+  },
+  render(h, { parent, slots, props }) {
+    const { default: defaultSlot = [], placeholder: placeholderSlot } = slots()
+
+    if (!parent._isMounted) {
+      return defaultSlot
+    }
+
+    parent.$once('hook:mounted', () => {
+      parent.$forceUpdate()
+    })
+
+    if (props.placeholderTag && (props.placeholder || placeholderSlot)) {
+      return h(
+        props.placeholderTag,
+        {
+          class: ['server-only-placeholder']
+        },
+        props.placeholder || placeholderSlot
+      )
+    }
+
+    // Return a placeholder element for each child in the default slot
+    // Or if no children return a single placeholder
+    return defaultSlot.length > 0 ? defaultSlot.map(() => h(false)) : h(false)
+  }
+}
